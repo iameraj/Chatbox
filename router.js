5e689714bc031ca1c8f59690d69121287c390c1f
iameraj <iameraj@outlook.com>
2023-11-25 11:48:03 +0530

    added middleware and functionality to add users

--- router.js
+++ router.js
@@ -1,25 +1,42 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-router
+const User = require('./models/user');
+
+router // This is two catch any external errors
+	.use((err, req, res, next) => {
+		console.error('Error:', err);
+		res.status(500).json({ error: 'Internal Server Error' });
+  });
+
+router // Login page
     .get('/login', (req, res) => {
-    res.send('This is login page')
-  })
+		res.send('This is login page')
+    })
     .post('/login', (req, res) => {
-    res.send('You have logged in :)')
-  });
+		res.send('You have logged in :)')
+    });
 
-router
+router // Signup page
     .get('/signup', (req, res) => {
-    res.send('This is signup page')
-  })
-    .post('/signup', (req, res) => {
-    res.send('You have signed in :)')
-  });
+		res.send('This is signup page')
+    })
+	.post('/signup', async (req, res) => {
+		try {
+			const { username, password } = req.body;
+			const newUser = new User({ username, password});
+			await newUser.save();
+			res.status(201).send("User created Successfully :)");
+		} catch (error) {
+			console.log('Error during signup:', error);
+			res.status(400).send("Bad Request :(");
+			return;
+		}
+	});
 
-router   
+router // Logut page
     .post('/logout', (req, res) => {
-    res.send('You have logged out :)')
-  });
+		res.send('You have logged out :)');
+    });
 
 module.exports = router;
