847963a589a80a06fde56817c5487eb9b5deef80
iameraj <iameraj@outlook.com>
2023-11-25 22:52:21 +0530

    users password are now encrypted

--- router.js
+++ router.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 
 const User = require('./models/user');
@@ -14,21 +16,27 @@ router // Login page
         res.send('This is login page')
     })
     .post('/login', async (req, res) => {
-        const { username, password } = req.body;
+        try {
+            const { username, password } = req.body;
+            const user = await User.findOne({ username });
+            if (!user) {
+                return res.status(401).json({ error: 'Invalid username or password' });
+            }
 
-        const user = await User.findOne({ username });
-        if (!user) {
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+                return res.status(200).json({ message: 'You have logged in :)' });
+            } 
             return res.status(401).json({ error: 'Invalid username or password' });
-          }
+                
 
-        const passwordMatch = (password === user.password) ? true:false;
-        if (passwordMatch) {
-            return res.status(200).json({ message: 'You have logged in :)' });
-          } 
-        else {
-            return res.status(401).json({ error: 'Invalid username or password' });
-          }
-        });
+        } catch (error) {
+            console.error('Error during login:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+    });
+        
 
 router // Signup page
     .get('/signup', (req, res) => {
