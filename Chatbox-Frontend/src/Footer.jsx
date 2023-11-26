a2ec13f99161731ce38a882b6d1189830ebca78b
iameraj <iameraj@outlook.com>
2023-11-26 12:43:38 +0530

    added a frontend

---
+++ Chatbox-Frontend/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-content">
+        <p>&copy; 2023 Chatbox by Iameraj</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ Chatbox-Frontend/src/Header.jsx
@@ -0,0 +1,15 @@
+
+
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-content">
+        <span className="chatbox-text">Chatbox</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
