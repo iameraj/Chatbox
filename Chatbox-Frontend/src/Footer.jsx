d0140196f82587083b8e9594e94403759f5a1edf
iameraj <iameraj@outlook.com>
2023-11-26 14:50:57 +0530

    reformatting

--- Chatbox-Frontend/src/Footer.jsx
+++ Chatbox-Frontend/src/Footer.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <div className="footer-container">
-      <div className="footer-content">
-        <p>&copy; 2023 Chatbox by Iameraj</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="footer-container">
+            <div className="footer-content">
+                <p>&copy; 2023 Chatbox by Iameraj</p>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
--- Chatbox-Frontend/src/Header.jsx
+++ Chatbox-Frontend/src/Header.jsx
@@ -1,15 +1,13 @@
-
-
-import './Header.css';
+import "./Header.css";
 
 function Header() {
-  return (
-    <div className="header-container">
-      <div className="header-content">
-        <span className="chatbox-text">Chatbox</span>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="header-container">
+            <div className="header-content">
+                <span className="chatbox-text">Chatbox</span>
+            </div>
+        </div>
+    );
+}
 
 export default Header;
