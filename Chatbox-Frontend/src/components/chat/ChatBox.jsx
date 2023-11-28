2cb322709ebb019aacdea28b3d0966a32ca506de
iameraj <iameraj@outlook.com>
2023-11-28 15:10:55 +0530

    added Chatbox component

---
+++ Chatbox-Frontend/src/components/chat/ChatBox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ChatBox.css";
+
+const ChatBox = () => {
+    return (
+        <div className="chat-box-container">
+            <div className="chat-box-content">
+                <p>You will see your chats here</p>
+            </div>
+        </div>
+    );
+};
+
+export default ChatBox;
