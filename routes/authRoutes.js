173c2d8a073d916d2e303f7b82e2c1d24e2fb627
iameraj <iameraj@outlook.com>
2023-11-30 16:15:09 +0530

    added routes and utils folder

---
+++ routes/authRoutes.js
@@ -0,0 +1,54 @@
+import { Router } from "express";
+import passport from "../utils/passport.js";
+const router = Router();
+
+// Login route
+router.post("/login", (req, res, next) => {
+    passport.authenticate("local", (err, user, _info) => {
+        if (err) {
+            return res.status(500).json({ error: "Internal Server Error lol" });
+        }
+
+        if (!user) {
+            return res.status(401).json({ error: "Authentication failed" });
+        }
+
+        req.logIn(user, (err) => {
+            if (err) {
+                return res.status(500).json({ error: "Internal Server Error" });
+            }
+            return res.status(200).json({ success: true, user: user });
+        });
+    })(req, res, next);
+});
+
+// Get user route
+router.get("/whoami", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.status(200).json({ authenticated: true, user: req.user });
+    } else {
+        res.status(401).json({ authenticated: false });
+    }
+});
+
+//Signup route
+router.post("/signup", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const newUser = new User({ username, password });
+        await newUser.save();
+        res.status(201).json({ success: true, user: newUser });
+    } catch (error) {
+        console.log("Error during signup:", error);
+        res.status(400).json({ error: "Bad Request" });
+        return;
+    }
+});
+
+// Logout route
+router.post("/logout", (req, res) => {
+    req.logout();
+    res.status(200).json({ success: true, message: "Logged out successfully" });
+});
+
+export default router;
---
+++ utils/passport.js
@@ -0,0 +1,41 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import User from "../models/user.js";
+import { compare } from "bcrypt";
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const user = await User.findOne({ username });
+            if (!user) {
+                return done(null, false);
+            }
+            const match = await compare(password, user.password);
+            if (!match) {
+                return done(null, false);
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        if (user) {
+            done(null, user);
+        } else {
+            done(new Error("User not found"));
+        }
+    } catch (error) {
+        done(error);
+    }
+});
+
+export default passport;
