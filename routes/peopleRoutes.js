42114b214e1ed7abb2cf8649a4ada1a0a2e6930a
iameraj <iameraj@outlook.com>
2023-12-10 17:43:09 +0530

    added endpoint to get list of all users

---
+++ routes/peopleRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import User from "../models/user.js";
+const router = express.Router();
+
+// List of all users
+router.get("/people", async (_req, res) => {
+	try {
+		const users = await User.find({}, "username _id");
+		return res.status(200).json(users);
+	} catch (error) {
+		console.error("Error fetching users:", error);
+		return res.status(500).json({ error: "Internal Server Error" });
+	}
+});
+
+export default router;
