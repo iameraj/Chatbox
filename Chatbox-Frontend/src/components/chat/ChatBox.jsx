ae15703840462e2dd32ab72f5fdca3f3f6c1f06c
iameraj <iameraj@outlook.com>
2024-01-04 17:08:48 +0530

    added switching feature for navbar

--- Chatbox-Frontend/src/components/chat/ChatBox.jsx
+++ Chatbox-Frontend/src/components/chat/ChatBox.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../navbar/Navbar";
 import "./ChatBox.css";
+import FriendsPage from "../pages/FriendsPage";
+import HomePage from "../pages/HomePage";
+import ProfilePage from "../pages/ProfilePage";
 
 const ChatBox = ({ userId }) => {
+	const [selectedPage, setSelectedPage] = useState("home");
+
+	const renderPage = () => {
+		switch (selectedPage) {
+			case "friends":
+				return <FriendsPage />;
+			case "profile":
+				return <ProfilePage />;
+			default:
+				return <HomePage />;
+		}
+	};
 	return (
 		<div className="chat-box-container">
-			<div className="chat-box-content">
-				<p>Hi {userId}!</p>
-				<p>You will see your chats here</p>
-			</div>
+			<Navbar onNavItemClicked={setSelectedPage} />
+			<div className="chat-box-content">{renderPage()}</div>
 		</div>
 	);
 };
