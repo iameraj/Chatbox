eb44f553e5230ed46e5bfb971a778393b2e6d6d9
iameraj <iameraj@outlook.com>
2024-01-10 20:47:11 +0530

    updated enpoint to return data in JSON format

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -10,7 +10,7 @@ router.get("/list", async (req, res) => {
 		if (!friendList) {
 			return res.status(200).json({ no_of_friends: "zero lol" });
 		}
-		return res.status(200).json(friendList);
+		return res.status(200).json({ friendIDs: friendList });
 	} catch (error) {
 		console.log("Internal server Error", error);
 		return res.status(500).json({ error: "Internal Server Error Lol" });
