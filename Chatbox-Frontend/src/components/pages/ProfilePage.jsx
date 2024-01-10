f9aa31c2201ca3a2a07205196193ca71ba4e3cea
iameraj <iameraj@outlook.com>
2024-01-10 16:30:22 +0530

    improved profile page

--- Chatbox-Frontend/src/components/pages/ProfilePage.jsx
+++ Chatbox-Frontend/src/components/pages/ProfilePage.jsx
@@ -5,6 +5,7 @@ const TARGET = "http://127.0.0.1:3002";
 function ProfilePage() {
 	const [user, setUser] = useState(null);
 	const [username, setUsername] = useState("User");
+	const [email, setEmail] = useState("Not provided");
 	const [image, setImage] = useState(null);
 
 	useEffect(() => {
@@ -18,6 +19,7 @@ function ProfilePage() {
 				const data = await response.json();
 				setUser(data.user);
 				setUsername(data.user.username);
+				setEmail(data.user.email);
 				setImage(data.user.profilePicture);
 			} catch (error) {
 				console.error("Error fetching user data:", error);
@@ -30,27 +32,34 @@ function ProfilePage() {
 	// Set background color based on the user's themeColor
 	return (
 		<div className="pages-container">
-			{image && (
-				<div
-					style={{
-						width: "100px", // Set your desired width and height
-						height: "100px",
-						borderRadius: "50%",
-						overflow: "hidden",
-					}}
-				>
-					<img
-						src={image}
-						alt="ProfilePicture"
+			<div style={{ display: "flex", alignItems: "center" }}>
+				{image && (
+					<div
 						style={{
-							width: "100%",
-							height: "100%",
-							objectFit: "cover",
+							width: "100px", // Set your desired width and height
+							height: "100px",
+							borderRadius: "50%",
+							overflow: "hidden",
+							margin: "10px",
 						}}
-					/>
+					>
+						<img
+							src={image}
+							alt="ProfilePicture"
+							style={{
+								width: "100%",
+								height: "100%",
+								objectFit: "cover",
+							}}
+						/>
+					</div>
+				)}
+				<div className="profileDetails">
+					{username} <br></br>
+					<small>{email}</small>
 				</div>
-			)}
-			<h2>Hi! {username}</h2>
+			</div>
+			<hr></hr>
 			Lorem ipsum dolor sit amet, officia excepteur ex fugiat
 			reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
 			ex non excepteur duis sunt velit enim. Voluptate laboris sint
