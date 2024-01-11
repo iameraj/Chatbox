5e7b86a77fe4af9a06aaf2bfe5899abc34cf3e24
iameraj <iameraj@outlook.com>
2024-01-11 11:46:22 +0530

    added more messaging routes

--- routes/messagingRoutes.js
+++ routes/messagingRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Chat from "../models/chat.js";
+import User from "../models/user.js";
 const router = express.Router();
 
 router.post("/send", async (req, res) => {
@@ -11,6 +12,10 @@ router.post("/send", async (req, res) => {
 			return res
 				.status(400)
 				.json({ error: "Receiver ID is required in the request body" });
+		}
+		const receiver = await User.findById(receiverId);
+		if (!receiver) {
+			return res.status(400).json({ error: "Invalid Receiver-Id" });
 		}
 
 		// Check if the
@@ -50,6 +55,69 @@ router.post("/send", async (req, res) => {
 		existingChat.messages.push(newMessage);
 		const savedChat = await existingChat.save();
 		res.status(200).json(savedChat);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+router.get("/getChat", async (req, res) => {
+	try {
+		const senderId = req.user._id;
+		const { receiverId } = req.body;
+
+		if (!receiverId) {
+			return res
+				.status(400)
+				.json({ error: "Receiver ID is required in the request body" });
+		}
+
+		const query = Chat.where({
+			$or: [
+				{ sender: senderId, receiver: receiverId },
+				{ sender: receiverId, receiver: senderId },
+			],
+		});
+		let existingChat = await query.findOne();
+
+		if (!existingChat) {
+			res.status(404).json({ message: "No chats were found" });
+		} else {
+			console.log("Older Chat thread was found");
+			res.status(200).json(existingChat);
+		}
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+router.post("/sendToChat", async (req, res) => {
+	try {
+		const senderId = req.user._id;
+		const { chatId, message } = req.body;
+
+		if (!chatId) {
+			return res
+				.status(400)
+				.json({ error: "ChatID is required in the request body" });
+		}
+
+		let existingChat = await Chat.findById(chatId);
+
+		if (!existingChat) {
+			return res.status(404).json({ message: "No chats were found" });
+		} else {
+			console.log("Older Chat thread was found");
+		}
+
+		const newMessage = {
+			sender: senderId,
+			message: message,
+			timestamp: new Date(),
+		};
+
+		existingChat.messages.push(newMessage);
+		await existingChat.save();
+		res.status(201).json();
 	} catch (error) {
 		res.status(500).json(error);
 	}
